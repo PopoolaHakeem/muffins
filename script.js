a23b68e5cc1e1664f468cbd16e66fd33324c945f
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-07-21 16:35:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const Menu = document.querySelector ('#menu');
+const Cancel = document.querySelector ('#cancel');
+const NavList = document.querySelector ('#mobile-list')
+
+Menu.onclick = function () {
+    Menu.style.display = 'none'
+    Cancel.style.display = 'block'
+    NavList.style.display = 'block'
+    
+}
+
+Cancel.onclick = function () {
+    Menu.style.display = 'block'
+    Cancel.style.display = 'none'
+    NavList.style.display = 'none'
+    
+}
