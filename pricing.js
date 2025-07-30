72ecda58a74d0677660c2bf3348a65a0210ed6fb
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-07-31 00:32:08 +0100

    updated index

---
+++ pricing.js
@@ -0,0 +1,19 @@
+
+
+var toggleBtn = document.getElementById ('toggle-button');
+var toggleIconOn = document.getElementById ('toggle-icon-on');
+var toggleIconOff = document.getElementById ('toggle-icon-off');
+
+let isToggle = false;
+
+toggleIconOn.onclick = function () {
+    toggleIconOn.style.display = 'none'
+    toggleIconOff.style.display = 'block'
+
+};
+
+toggleIconOff.onclick = function () {
+    toggleIconOn.style.display = 'block'
+    toggleIconOff.style.display = 'none'
+
+};
